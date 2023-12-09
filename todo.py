06624dd60f87391d7f9212b5a019593b8150271c
Abhyas Mall <mallabhyas@gmail.com>
2023-12-09 04:06:55 -0600

    Enhanced

--- todo.py
+++ todo.py
@@ -110,7 +110,7 @@ class Tasks:
             print("No tasks to list.")
             return
         print("Tasks to list:")
-        tasks_to_list.sort(key=lambda x: (x.due_date if x.due_date else date.min, x.priority, x.created), reverse=True)
+        tasks_to_list.sort(key=lambda x: (x.due_date, -x.priority, x.created), reverse=False)
         self.display_tasks(tasks_to_list, list_view=True)
 
     def report_tasks(self):
@@ -200,7 +200,7 @@ class Tasks:
         if not tasks_to_query:
             print("No matching tasks found.")
             return
-        tasks_to_query.sort(key=lambda x: (x.due_date if x.due_date else datetime.min, x.priority), reverse=True)
+        tasks_to_query.sort(key=lambda x: (x.due_date, -x.priority), reverse=False)
         self.display_tasks(tasks_to_query, list_view=True)
 
 def get_priority_input():
